9799c13accd65076fab6e2b75946b1b0f62813da
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-06-15 16:34:54 -0700

    added images and newItem and newImage functions

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+//function declaration for newImage
+function newImage (ImageSource,ImagePosition, ImageLeft, ImageBottom){let Image = document.createElement('img')
+
+Image.src = ImageSource;
+Image.style.position = ImagePosition;
+Image.style.left = ImageLeft;
+Image.style.bottom = ImageBottom;
+document.body.appendChild(Image);
+}
+
+// creating the character using newImage function
+newImage("assets/green-character.gif", "fixed", "100px", "100px");
+
+// creating the pine tree using newImage function
+newImage("assets/pine-tree.png","fixed", "450px", "200px");
+
+// creating the tree using newImage function
+newImage("assets/tree.png","fixed", "200px", "300px");
+
+// creating the pillar using newImage function
+newImage("assets/pillar.png","fixed", "350px", "100px");
+
+// creating the crate using newImage function
+newImage("assets/crate.png","fixed", "150px", "200px");
+
+// creating the well using newImage function
+newImage("assets/well.png","fixed", "5000px", "450px");
+
+
+//function declaration for newItem
+function newItem (ImageSource,ImagePosition, ImageLeft, ImageBottom) {let Item = newImage (ImageSource,ImagePosition, ImageLeft, ImageBottom); 
+
+Item.addEventListener('dblclick', function (){
+    Item.remove() });
+
+}
+
+// creating the sword using newItem function
+
+newItem("assets/sword.png", "fixed", "500px", "405px")
+
+// creating the sheild using newItem function
+
+newItem("assets/sheild.png", "fixed", "165px", "185px")
+
+// creating the staff using newItem function
+    
+newItem("assets/staff.png", "fixed", "600px", "100px")
+
